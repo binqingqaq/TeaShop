d652e0b12ec7c8d68b8c1f8d919518bcbfdd39f7
Binqing <3519958191@qq.com>
2022-04-16 22:54:49 +0800

    bi update User .java

--- src/com/bin/git/Student.java
+++ src/com/bin/git/Student.java
@@ -3,6 +3,9 @@ package com.bin.git;
 public class Student {
     public static void main(String[] args) {
         System.out.println("我是一只小雅小苹果");
+
+
+        System.out.println("bi分支进行操作");
     }
 
 }
